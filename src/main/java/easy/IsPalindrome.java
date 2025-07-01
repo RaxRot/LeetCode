1e78599a1f8a584f8389527828deece0602f5b26
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-01 21:52:14 +0100

    add IsPalindrome

---
+++ src/main/java/easy/IsPalindrome.java
@@ -0,0 +1,19 @@
+package easy;
+
+//Given an integer x, return true if x is a palindrome, and false otherwise.
+public class IsPalindrome {
+    public boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+        int start = 0;
+        int end = str.length() - 1;
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+}
