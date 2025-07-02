00f17c7e02c7cf0ddcfa0663a7c8cb31d8f3533e
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-02 13:06:31 +0100

    add SingleNumber

---
+++ src/main/java/easy/SingleNumber.java
@@ -0,0 +1,24 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            }else{
+                map.put(num, 1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}
