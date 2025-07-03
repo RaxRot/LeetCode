176641dae236ca909806b03869436ae514089838
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-03 13:56:04 +0100

    add MoveZeroes

---
+++ src/main/java/easy/MoveZeroes.java
@@ -0,0 +1,17 @@
+package easy;
+//Given an integer array nums, move all 0's to the end of it while maintaining
+// the relative order of the non-zero elements.
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        for (int i = index; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
