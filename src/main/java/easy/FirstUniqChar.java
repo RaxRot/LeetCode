221fd736129483f2c4cf8d53e2cc07b4feffb00c
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-03 14:00:33 +0100

    add FirstUniqChar

---
+++ src/main/java/easy/FirstUniqChar.java
@@ -0,0 +1,25 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Given a string s, find the first non-repeating character in it and return its index.
+// If it does not exist, return -1.
+public class FirstUniqChar {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+            }else{
+                map.put(s.charAt(i), 1);
+            }
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
