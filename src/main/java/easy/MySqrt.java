c2b7a5f516e9c8ebd3df21588c0052414cdf5a39
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-03 14:57:41 +0100

    add MySqrt

---
+++ src/main/java/easy/MySqrt.java
@@ -0,0 +1,9 @@
+package easy;
+//Given a non-negative integer x, return the square root of x rounded down to the nearest integer.
+// The returned integer should be non-negative as well.
+public class MySqrt {
+    public int mySqrt(int x) {
+        int squareRoot = (int) Math.sqrt(x);
+        return squareRoot;
+    }
+}
